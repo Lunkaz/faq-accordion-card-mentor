b171768fb9997b44ed207a3a805db145d3c10e4b
Lunkaz <marialbornozapata@gmail.com>
2021-04-15 15:11:08 -0300

    Finally final version

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
-const $AllQuestions = document.querySelectorAll(".faq-question-container");
-const $AllAnswers = document.querySelectorAll(".faq-answer")
+const $allQuestions = document.querySelectorAll(".faq-question-container");
+const $allAnswers = document.querySelectorAll(".faq-answer");
+const $allTitles = document.querySelectorAll(".faq-question-title");
 
 //console.log($AllQuestions)
 
-function hideAllAnswers() {
-    for (let answer of $AllAnswers) {
-        answer.classList.remove("active");
+function openQuestion(question) {
+    question.querySelector(".faq-answer").classList.add("active");
+    question.querySelector(".faq-arrow-icon").style.transform = "rotate(180deg)";
+    question.querySelector(".faq-question-title").style.fontWeight = 700;
+}
+
+function closeQuestion(question) {
+    question.querySelector(".faq-answer").classList.remove("active");
+    question.querySelector(".faq-arrow-icon").style.transform = "none";
+    question.querySelector(".faq-question-title").style.fontWeight = 400;
+}
+
+function closeAll(question, isActive) {
+    if (isActive) {
+        question.querySelector(".faq-answer").classList.remove("active");
     }
 }
 
+let lastQuestion = null;
 
+function closeLast() {
+    if (lastQuestion) {
+        closeQuestion(lastQuestion);
+    }
 
-function showAnswer(e){
-    hideAllAnswers()
-    for (let i = 0; i < $AllQuestions.length; i++) {
-        if (e.target.innerText === $AllQuestions[i].innerText) {
-        $AllAnswers[i].classList.add("active"); 
-        }
-    } 
 }
 
-$AllQuestions[0].addEventListener("click", showAnswer);
-$AllQuestions[1].addEventListener("click", showAnswer);
-$AllQuestions[2].addEventListener("click", showAnswer);
-$AllQuestions[3].addEventListener("click", showAnswer);
-$AllQuestions[4].addEventListener("click", showAnswer);
+for (let i = 0; i < $allQuestions.length; i++) {
+
+    $allQuestions[i].addEventListener("click", function () {
+        closeLast();
+        if ($allQuestions[i] === lastQuestion) {
+            lastQuestion = null;
+        } else {
+            openQuestion($allQuestions[i]);
+            lastQuestion = $allQuestions[i];
+        }
+       
+    })
+
+}
+
+
+
